bdd1873735adafd950ccd172c507090b8ed02ae8
GITA <jgitanjali592@gmail.com>
2024-03-02 12:48:00 +0530

    this code for react fragment

--- index.js
+++ index.js
@@ -6,9 +6,19 @@
 
 // document.getElementById("root").append(heading);
 
-// react
+// react with js and jsx
 
 // const reactHeading = React.createElement("h1" , {className:"head" , id:"reactHead" ,children:"Hello React"});
-const jsxHeading = (<h1 classname="head">Hello Jsx</h1>)
-ReactDOM.createRoot(document.getElementById("root")).render(jsxHeading);
+// const jsxHeading = <h1 classname="head">Hello Jsx</h1>
+// ReactDOM.createRoot(document.getElementById("root")).render(jsxHeading);
 // console.log("React element :",reactHeading);
+
+
+
+// React fragment
+const jsxHeading = <React.Fragment>
+
+    <h1 classname="head">Hello Jsx</h1>
+    <p>this is created using jsx</p>
+</React.Fragment>
+    ReactDOM.createRoot(document.getElementById("root")).render(jsxHeading);
